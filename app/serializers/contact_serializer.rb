86a9e02fd3b3a652bc333f9dcd22d685bdc92e08
demershow <demersontorres520@gmail.com>
2023-01-08 19:26:09 -0300

    Aula 49: HTTP authentication

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -7,8 +7,11 @@ class ContactSerializer < ActiveModel::Serializer
 
   has_many :phones do
     link(:related) { contact_kind_url(object.id) }
+    link(:related) { contact_phones_url(object.id) }
   end
-  has_one :address
+  has_one :address do
+    link(:related) { contact_address_url(object.id) }
+  end
 
   link(:self) { contact_url(object.id) }
   link(:related) { kind_url(object.kind.id) }
