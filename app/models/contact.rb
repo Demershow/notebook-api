af9edc55f2b7201c0409ef1aa31cdbb0e55998bd
demershow <demersontorres520@gmail.com>
2023-01-08 22:38:44 -0300

    Aula 81: getting hash class better

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,12 @@
 class Contact < ApplicationRecord
 
+   #Validations
+   validates_presence_of :kind
+   validates_presence_of :address
+
+  # Kaminari
+  paginates_per 5
+
   # Associations
   belongs_to :kind #, optional: true
   has_many :phones
